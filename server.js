8db701c557a8bce685e2000aa5ef35b87c57d160
Jonathan Price <jonprice0@gmail.com>
2022-07-27 11:27:34 -0400

    added helper util for handlebars

--- server.js
+++ server.js
@@ -23,6 +23,13 @@ const sess = {
 
 app.use(session(sess));
 
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
